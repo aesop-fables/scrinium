7ba4eaedd334c32c47e076c3227ea4e05899c223
Josh Arnold <rncodepoet@gmail.com>
2023-10-11 07:17:07 -0500

    Bump version to 0.6.5

--- deploy-local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-yarn && yarn build
-yarn rimraf ../../guidedchoice/data-modules/node_modules/@aesop-fables/scrinium/lib
-cp -R ./lib ../../guidedchoice/data-modules/node_modules/@aesop-fables/scrinium/lib
