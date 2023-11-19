01af61bbf610e6c19b015c01438e1252e1056961
Josh Arnold <rncodepoet@gmail.com>
2023-11-19 14:44:59 -0600

    Add support for predicate filters for subjects

---
+++ src/Predicate.ts
@@ -0,0 +1,23 @@
+import { ISubject } from './ISubject';
+
+export declare type Predicate = ISubject<boolean>;
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+declare type Constructor = Function;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare type PredicateIdentifier = string;
+
+const metadataKey = Symbol('@aesop-fables/scrinium/predicates/metadata');
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const getPredicateMetadata = (constructor: Constructor) => {
+  const metadata = Reflect.getMetadata(metadataKey, constructor);
+  console.log('metadata', metadata);
+  return metadata as PredicateIdentifier | undefined;
+};
+
+export function predicate(lookup: PredicateIdentifier) {
+  return function (target: Constructor) {
+    Reflect.defineMetadata(metadataKey, lookup, target);
+  };
+}
