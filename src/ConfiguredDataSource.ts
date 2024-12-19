4467a854301f5422bf6657abf74aaf9952ba0237
Josh Arnold <rncodepoet@gmail.com>
2024-12-19 13:29:31 -0600

    Lint fixes

--- src/ConfiguredDataSource.ts
+++ src/ConfiguredDataSource.ts
@@ -9,7 +9,7 @@ export class ConfiguredDataSource<T> implements IDataCompartmentSource<T> {
    * Constructs a new configured data source.
    * @param provider The function to invoke to resolve the data for the compartment.
    */
-  constructor(private readonly provider: () => Promise<T>) { }
+  constructor(private readonly provider: () => Promise<T>) {}
   /**
    * Loads the data for a compartment from the provider.
    */
