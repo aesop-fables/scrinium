f9414bc204bfd19179710e22ffb032068cf6ff9a
Josh Arnold <rncodepoet@gmail.com>
2025-01-23 10:35:38 -0600

    Add methods to clear data caches and repositories, and build state in DataStore

--- src/DataStore.ts
+++ src/DataStore.ts
@@ -1,12 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { DataCache } from './DataCache';
+import { combineLatest, map, Observable, of } from 'rxjs';
+import { DataCache, IDataCache } from './DataCache';
 import { DataStoreToken } from './DataStoreToken';
 import { ICompartmentStorage } from './ICompartmentStorage';
-import { IRepository } from './Repository';
+import { IRepository, Repository } from './Repository';
 
 // Essentially an in-memory database
 // First pass is JUST a replacement for AppStorage
 // Then we'll layer in the schema and triggers
+
+export type DataStoreState = {
+  dataCaches: IDataCache[];
+  repositories: IRepository<any>[];
+};
+
+/**
+ * Provides a managed data store for caching and repositories as well as managing their relationships.
+ */
 export class DataStore {
   constructor(private readonly dataCatalog: DataCatalog) {}
 
@@ -16,6 +26,40 @@ export class DataStore {
 
   public repository<Registry>(token: DataStoreToken): IRepository<Registry> {
     return this.dataCatalog.get(token) as IRepository<Registry>;
+  }
+
+  async clearDataCaches(): Promise<void> {
+    const { caches } = this.dataCatalog;
+    for (let i = 0; i < caches.length; i++) {
+      const cache = caches[i] as DataCache<any>;
+      await cache.resetAll();
+    }
+  }
+
+  async clearRepositories(): Promise<void> {
+    const { repositories } = this.dataCatalog;
+    for (let i = 0; i < repositories.length; i++) {
+      const repository = repositories[i] as Repository<any>;
+      await repository.reset();
+    }
+  }
+
+  async clearAll(): Promise<void> {
+    await Promise.all([this.clearRepositories(), this.clearDataCaches()]);
+  }
+
+  get state$(): Observable<DataStoreState> {
+    const caches = of(this.dataCatalog.caches);
+    const repositories = of(this.dataCatalog.repositories);
+
+    return combineLatest([repositories, caches]).pipe(
+      map(([repositories, values]) => {
+        return {
+          dataCaches: Object.entries(values).map(([, dataCache]) => dataCache),
+          repositories: Object.entries(repositories).map(([, repository]) => repository),
+        };
+      }),
+    );
   }
 }
 
@@ -28,6 +72,18 @@ type Catalog = {
 
 export class DataCatalog {
   private readonly values: Map<DataStoreToken, Catalog> = new Map();
+
+  public get caches() {
+    return Array.from(this.values.values())
+      .filter((x) => x.type === 'cache')
+      .map((x) => x.storage as DataCache<any>);
+  }
+
+  public get repositories() {
+    return Array.from(this.values.values())
+      .filter((x) => x.type === 'repository')
+      .map((x) => x.storage as IRepository<any>);
+  }
 
   public registerCache<Compartments>(token: DataStoreToken, cache: DataCache<Compartments>): void {
     this.values.set(token, { type: 'cache', storage: cache });
--- src/__tests__/DataStore.test.ts
+++ src/__tests__/DataStore.test.ts
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { mock } from 'jest-mock-extended';
 import { DataCompartmentOptions } from '../Compartments';
 import { ConfiguredDataSource } from '../ConfiguredDataSource';
-import { createDataCache } from '../DataCache';
+import { createDataCache, DataCache } from '../DataCache';
 import { DataCatalog, DataStore } from '../DataStore';
 import { DataStoreToken } from '../DataStoreToken';
-import { createRepository } from '../Repository';
+import { createRepository, Repository } from '../Repository';
+import { firstValueFrom } from 'rxjs';
 
 describe('DataStore', () => {
   const testToken = new DataStoreToken('test');
@@ -47,6 +49,116 @@ describe('DataStore', () => {
     const values = new DataCatalog();
     const store = new DataStore(values);
     expect(store.repository(testToken)).toBeUndefined();
+  });
+
+  test('Clears all data caches', async () => {
+    const cacheToken1 = new DataStoreToken('cache1');
+    const cacheToken2 = new DataStoreToken('cache2');
+    const repoToken1 = new DataStoreToken('repo1');
+    const repoToken2 = new DataStoreToken('repo2');
+
+    const cache1 = mock<DataCache<TestCompartments>>();
+    const cache2 = mock<DataCache<TestCompartments>>();
+    const repo1 = mock<Repository<TestCompartments>>();
+    const repo2 = mock<Repository<TestCompartments>>();
+
+    const values = new DataCatalog();
+    values.registerCache(cacheToken1, cache1);
+    values.registerCache(cacheToken2, cache2);
+    values.registerRepository(repoToken1, repo1);
+    values.registerRepository(repoToken2, repo2);
+
+    const store = new DataStore(values);
+    await store.clearDataCaches();
+
+    expect(cache1.resetAll).toHaveBeenCalled();
+    expect(cache2.resetAll).toHaveBeenCalled();
+    
+    expect(repo1.reset).not.toHaveBeenCalled();
+    expect(repo2.reset).not.toHaveBeenCalled();
+  });
+
+  test('Clears all repositories', async () => {
+    const cacheToken1 = new DataStoreToken('cache1');
+    const cacheToken2 = new DataStoreToken('cache2');
+    const repoToken1 = new DataStoreToken('repo1');
+    const repoToken2 = new DataStoreToken('repo2');
+
+    const cache1 = mock<DataCache<TestCompartments>>();
+    const cache2 = mock<DataCache<TestCompartments>>();
+    const repo1 = mock<Repository<TestCompartments>>();
+    const repo2 = mock<Repository<TestCompartments>>();
+
+    const values = new DataCatalog();
+    values.registerCache(cacheToken1, cache1);
+    values.registerCache(cacheToken2, cache2);
+    values.registerRepository(repoToken1, repo1);
+    values.registerRepository(repoToken2, repo2);
+
+    const store = new DataStore(values);
+    await store.clearRepositories();
+
+    expect(repo1.reset).toHaveBeenCalled();
+    expect(repo2.reset).toHaveBeenCalled();
+
+    expect(cache1.resetAll).not.toHaveBeenCalled();
+    expect(cache2.resetAll).not.toHaveBeenCalled();
+  });
+
+  test('Clears all caches and repositories', async () => {
+    const cacheToken1 = new DataStoreToken('cache1');
+    const cacheToken2 = new DataStoreToken('cache2');
+    const repoToken1 = new DataStoreToken('repo1');
+    const repoToken2 = new DataStoreToken('repo2');
+
+    const cache1 = mock<DataCache<TestCompartments>>();
+    const cache2 = mock<DataCache<TestCompartments>>();
+    const repo1 = mock<Repository<TestCompartments>>();
+    const repo2 = mock<Repository<TestCompartments>>();
+
+    const values = new DataCatalog();
+    values.registerCache(cacheToken1, cache1);
+    values.registerCache(cacheToken2, cache2);
+    values.registerRepository(repoToken1, repo1);
+    values.registerRepository(repoToken2, repo2);
+
+    const store = new DataStore(values);
+    await store.clearAll();
+
+    expect(repo1.reset).toHaveBeenCalled();
+    expect(repo2.reset).toHaveBeenCalled();
+
+    expect(cache1.resetAll).toHaveBeenCalled();
+    expect(cache2.resetAll).toHaveBeenCalled();
+  });
+
+  test('Builds up state', async () => {
+    const cacheToken1 = new DataStoreToken('cache1');
+    const cacheToken2 = new DataStoreToken('cache2');
+    const repoToken1 = new DataStoreToken('repo1');
+    const repoToken2 = new DataStoreToken('repo2');
+
+    const cache1 = mock<DataCache<TestCompartments>>();
+    const cache2 = mock<DataCache<TestCompartments>>();
+    const repo1 = mock<Repository<TestCompartments>>();
+    const repo2 = mock<Repository<TestCompartments>>();
+
+    const values = new DataCatalog();
+    values.registerCache(cacheToken1, cache1);
+    values.registerCache(cacheToken2, cache2);
+    values.registerRepository(repoToken1, repo1);
+    values.registerRepository(repoToken2, repo2);
+
+    const store = new DataStore(values);
+    const { state$ } = store;
+    const state = await firstValueFrom(state$);
+    expect(state.dataCaches.length).toBe(2);
+    expect(state.dataCaches[0]).toBe(cache1);
+    expect(state.dataCaches[1]).toBe(cache2);
+
+    expect(state.repositories.length).toBe(2);
+    expect(state.repositories[0]).toBe(repo1);
+    expect(state.repositories[1]).toBe(repo2);
   });
 
   type TestCompartments = {
