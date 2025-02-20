568d3d2eec66fee5a2c17506ea1d1207f5ad63c1
Josh Arnold <rncodepoet@gmail.com>
2025-02-20 08:42:48 -0600

    Add the wait for cache predicate

--- src/Metadata.ts
+++ src/Metadata.ts
@@ -1,13 +1,14 @@
 import { IServiceContainer } from '@aesop-fables/containr';
-import { Observable } from 'rxjs';
+import { combineLatest, map, Observable } from 'rxjs';
 import { DataStore } from './DataStore';
+import { DataStoreToken } from './DataStoreToken';
 
 export type ObservedPredicate = Observable<boolean>;
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type Constructor = Function;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type PredicateIdentifier = string;
+export type PredicateIdentifier = string | IMetadataSubject<any>;
 
 export type ConstructorDecorator = (target: Constructor) => void;
 
@@ -33,9 +34,31 @@ export interface IMetadataSubject<T> {
   createObservable(context: MetadataSubjectContext): Observable<T>;
 }
 
-// export function waitForCache(token: DataStoreToken) {
-//   return function (target: Constructor) {
-//     const metadata = getPredicateMetadata(target);
-//     // Reflect.defineMetadata(metadataKey, [...metadata, lookup], target);
-//   };
-// }
+// This is the decorator shim
+export function waitForCache<Compartments>(token: DataStoreToken): ConstructorDecorator {
+  return function (target: Constructor) {
+    const metadata = getPredicateMetadata(target);
+    const resolver = new WaitForCacheMetadataPredicate<Compartments>(token, []);
+    setPredicateMetadata(target, [...metadata, resolver]);
+  };
+}
+
+export type KeysOf<T> = (keyof T)[];
+
+export class WaitForCacheMetadataPredicate<Compartments> implements IMetadataSubject<boolean> {
+  constructor(
+    readonly token: DataStoreToken,
+    readonly keys: KeysOf<Compartments>,
+  ) {}
+
+  createObservable(context: MetadataSubjectContext): Observable<boolean> {
+    const { store } = context;
+    const cache = store.cache<Compartments>(this.token);
+    if (this.keys.length === 0) {
+      return cache.initialized$;
+    }
+
+    const filteredCompartments = cache.compartments.filter((x) => this.keys.includes(x.key as keyof Compartments));
+    return combineLatest(filteredCompartments.map((c) => c.initialized$)).pipe(map((x) => x.every((y) => y)));
+  }
+}
